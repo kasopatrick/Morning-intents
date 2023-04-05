6f8141c2204e2c511dc4ce7968b7120bf69bdcd0
kasopatrick <kasopatrick5@gmail.com>
2023-04-05 08:35:08 +0300

    first commit

---
+++ app/src/main/java/com/example/morningintents/WesiteActivity.kt
@@ -0,0 +1,11 @@
+package com.example.morningintents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WebsiteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wesite)
+    }
+}
